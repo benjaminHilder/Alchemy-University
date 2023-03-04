bd7b0298cc8a105b9bb80c4365a2e04b4c5d4040
Benjamin Hilder <benhilder7@gmail.com>
2023-03-04 19:52:48 +1030

    work started on nft page

--- Week 3 - blockexplorer/src/Pages/NFTHoldings.js
+++ Week 3 - blockexplorer/src/Pages/NFTHoldings.js
@@ -1,3 +1,49 @@
+import "../css/nftHoldings.css"
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
+
+//alchemy
+const settings = {
+    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+//alchemy
+
 export const NFTHoldings = () => {
-    return <h1> THIS IS THE NFT HOLDINGS PAGE</h1>
+    const [nftContractAddresses, setNftContractAddresses] = useState()
+    const [nftIds, setNftIds] = useState()
+    const [nftUris, setNftUris] = useState()
+
+    useEffect(async() => {
+        if (nftContractAddresses === undefined || nftIds === undefined || nftUris === undefined) {
+
+        const searchInput = localStorage.getItem("searchInput")
+
+        const result = await alchemy.nft.getNftsForOwner(searchInput)
+        const nfts = result.ownedNfts
+
+        const newNftContractAddresses = []
+        const newNftIds = []
+        const newNftUris = []
+
+        for (let i = 0; i < nfts.length; i++) {
+            newNftContractAddresses.push(nfts[i].contract.address)
+            newNftIds.push(nfts[i].tokenId)
+            newNftUris.push(nfts[i].tokenUri)
+        }
+
+        setNftContractAddresses(newNftContractAddresses)
+        setNftIds(newNftIds)
+        setNftUris(newNftUris)
+    }
+    })
+
+    return (
+        <div className="main">
+            {}
+        </div>
+        
+    )
 }
