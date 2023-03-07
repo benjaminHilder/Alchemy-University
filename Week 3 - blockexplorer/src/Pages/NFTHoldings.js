f880a91467be7592a9e25a03c33c2f8fc68c3d13
Benjamin Hilder <benhilder7@gmail.com>
2023-03-07 18:08:50 +1030

    Display nft images

--- Week 3 - blockexplorer/src/Pages/NFTHoldings.js
+++ Week 3 - blockexplorer/src/Pages/NFTHoldings.js
@@ -11,38 +11,54 @@ const settings = {
 const alchemy = new Alchemy(settings);
 //alchemy
 
+function displayNftImage(imageURL) {
+    return (
+        <div>
+            <img src = {imageURL} alt="nftImage"/>
+        </div>
+
+    )
+}
+
 export const NFTHoldings = () => {
     const [nftContractAddresses, setNftContractAddresses] = useState()
     const [nftIds, setNftIds] = useState()
-    const [nftUris, setNftUris] = useState()
+    const [nftImageURL, setNftImageURL] = useState()
 
     useEffect(async() => {
-        if (nftContractAddresses === undefined || nftIds === undefined || nftUris === undefined) {
+        if (nftContractAddresses === undefined || nftIds === undefined) {
 
         const searchInput = localStorage.getItem("searchInput")
 
         const result = await alchemy.nft.getNftsForOwner(searchInput)
-        const nfts = result.ownedNfts
+        const nfts = await result.ownedNfts
 
         const newNftContractAddresses = []
         const newNftIds = []
-        const newNftUris = []
+        const newNftImageURL = []
+
+        console.log(`${nfts}`)
 
         for (let i = 0; i < nfts.length; i++) {
-            newNftContractAddresses.push(nfts[i].contract.address)
-            newNftIds.push(nfts[i].tokenId)
-            newNftUris.push(nfts[i].tokenUri)
+            await newNftContractAddresses.push(nfts[i].contract.address)
+            await newNftIds.push(nfts[i].tokenId)
+
+            const nftMetadata = await alchemy.nft.getNftMetadata(nfts[i].contract.address, nfts[i].tokenId)
+
+            if (nftMetadata.media[0] != undefined) {
+                newNftImageURL.push(nftMetadata.media[0].gateway)
+            }
         }
 
         setNftContractAddresses(newNftContractAddresses)
         setNftIds(newNftIds)
-        setNftUris(newNftUris)
+        setNftImageURL(newNftImageURL)
     }
     })
 
     return (
         <div className="main">
-            {}
+            {nftImageURL && nftImageURL.map(image => displayNftImage(image))}
         </div>
         
     )
--- Week 3 - blockexplorer/src/Pages/TokenHoldings.js
+++ Week 3 - blockexplorer/src/Pages/TokenHoldings.js
@@ -1,3 +1,45 @@
+import "../css/nftHoldings.css"
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
+
+//alchemy
+const settings = {
+    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+//alchemy
+
 export const TokenHoldings = () => {
-    return <h1> THIS IS THE TOKEN HOLDINGS PAGE</h1>
+    const [tokenContractAddresses, setTokenContractAddresses] = useState()
+    const [tokenBalances, setTokenBalances] = useState()
+
+    useEffect(async() => {
+
+        const searchInput = localStorage.getItem("searchInput")
+
+        const tokenData = await alchemy.core.getTokenBalances(searchInput)
+        const tokenBalances = tokenData.tokenBalances
+
+
+        const newTokenContractAddresses = []
+        const newTokenBalances = []
+
+        for (let i = 0; i < tokenBalances.length; i++) {
+            newTokenContractAddresses.push(tokenBalances[i].contractAddress)
+            newTokenBalances.push(tokenBalances[i].tokenBalance)
+        }
+
+        setTokenContractAddresses(newTokenContractAddresses)
+        setTokenBalances(newTokenBalances)
+    })
+
+    return (
+        <div className="main">
+            {tokenContractAddresses}
+            {tokenBalances}
+        </div>
+        
+    )
 }
